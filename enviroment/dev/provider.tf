344f5b15e3f6f9fdab5c955c802291dd1cca9a87
Abdur Rub <abdurrub4u4@gmail.com>
2026-03-05 20:45:12 +0530

    Updated location value

--- enviroment/dev/provider.tf
+++ enviroment/dev/provider.tf
@@ -1,16 +1,17 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.4.0"
     }
   }
 }
 
 provider "azurerm" {
-    features {
-    
-    }
-  
-  subscription_id =  "6b085240-a951-404c-8e9c-110ec63f18de"
+  features {
+
+  }
+
+  subscription_id = "0b8a345f-5bb0-4af0-89af-77476d7c9b6f"
 }
+# "6b085240-a951-404c-8e9c-110ec63f18de"
